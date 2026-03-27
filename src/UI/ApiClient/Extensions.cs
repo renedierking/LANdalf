13ec3469a396092ae45e1069b5f2f9bdc17cdc01
renedierking <rdierking56@googlemail.com>
2026-03-27 21:41:05 +0100

    enhancing UI + user experience; refactoring

---
+++ src/UI/ApiClient/Extensions.cs
@@ -0,0 +1,14 @@
+﻿namespace LANdalf.UI.ApiClient {
+    public static class Extensions {
+        public static PcDeviceDTO Clone(this PcDeviceDTO dTO) {
+            return new PcDeviceDTO {
+                Id = dTO.Id,
+                Name = dTO.Name,
+                MacAddress = dTO.MacAddress,
+                IpAddress = dTO.IpAddress,
+                BroadcastAddress = dTO.BroadcastAddress,
+                IsOnline = dTO.IsOnline
+            };
+        }
+    }
+}
